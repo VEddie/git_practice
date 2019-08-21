ad4b1c734c1ac07f9f2cc9eed53c8a08ae578dd5
VEddie <edijose18@gmail.com>
2019-08-21 16:21:31 -0400

    Added set methods to the Cat Object.

--- script.js
+++ script.js
@@ -16,6 +16,21 @@ class Cat {
 	get color() {
 		return this._color;
 	}
+
+	set name(newName) {
+		if(typeof newName === 'string')
+			this._name = newName;
+	}
+
+	set age(newAge) {
+		if(typeof newAge === 'number')
+			this._age = newAge;
+	}
+
+	set color(newColor) {
+		if(typeof newColor === 'string')
+			this._color = newColor;
+	}
 }
 
 const testCat = new Cat('Eddie', 5, 'White');
